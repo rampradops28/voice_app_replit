8d9c4c0cbf096895b718a2864cc5faa19603fc9a
rampradops28 <rampradop28@gmail.com>
2025-08-23 10:56:34 +0530

    code files added

--- server/controllers/billController.js
+++ server/controllers/billController.js
@@ -1,4 +1,5 @@
 import { storage } from "../storage.js";
+import { isTwilioConfigured, sendSms } from "../services/smsService.js";
 
 export const getBill = async (req, res) => {
   try {
@@ -14,6 +15,18 @@ export const getBill = async (req, res) => {
 export const upsertBill = async (req, res) => {
   try {
     const bill = await storage.createOrUpdateBill(req.body);
+
+    // Fire-and-forget SMS notification if configured and phone present
+    const phone = bill.customerPhone || req.body.customerPhone;
+    if (phone && isTwilioConfigured()) {
+      const amount = bill.totalAmount ?? req.body.totalAmount;
+      const session = bill.sessionId;
+      const msg = `Your bill (${session}) total is ${amount}. Thank you!`;
+      sendSms({ to: phone, body: msg }).catch((e) => {
+        console.error("SMS send failed:", { message: e.message, code: e.code });
+      });
+    }
+
     res.json(bill);
   } catch (error) {
     console.error("Error creating/updating bill:", error);
