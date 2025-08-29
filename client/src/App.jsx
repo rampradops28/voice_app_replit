5a98439d7ca1cdc0f6564c40118f7227953102dc
rampradops28 <rampradop28@gmail.com>
2025-08-29 11:48:20 +0530

    small update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/Toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { useAuth } from "@/hooks/UseAuth";
 import LoginPage from "./pages/Login";
